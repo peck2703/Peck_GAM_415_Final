875e056a69c31b8071f71df5ce40fded994c38a1
peck2703 <mrpeckjr@gmail.com>
2019-01-27 14:12:49 -0600

    Added TreeActors, so far no crash.

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/Peck_GAM_415/TreeActor.generated.h
@@ -0,0 +1,100 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PECK_GAM_415_TreeActor_generated_h
+#error "TreeActor.generated.h already included, missing '#pragma once' in TreeActor.h"
+#endif
+#define PECK_GAM_415_TreeActor_generated_h
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_RPC_WRAPPERS
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATreeActor(); \
+	friend PECK_GAM_415_API class UClass* Z_Construct_UClass_ATreeActor(); \
+public: \
+	DECLARE_CLASS(ATreeActor, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Peck_GAM_415"), NO_API) \
+	DECLARE_SERIALIZER(ATreeActor) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATreeActor(); \
+	friend PECK_GAM_415_API class UClass* Z_Construct_UClass_ATreeActor(); \
+public: \
+	DECLARE_CLASS(ATreeActor, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Peck_GAM_415"), NO_API) \
+	DECLARE_SERIALIZER(ATreeActor) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATreeActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATreeActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATreeActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATreeActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATreeActor(ATreeActor&&); \
+	NO_API ATreeActor(const ATreeActor&); \
+public:
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATreeActor(ATreeActor&&); \
+	NO_API ATreeActor(const ATreeActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATreeActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATreeActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATreeActor)
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__m_rootBase() { return STRUCT_OFFSET(ATreeActor, m_rootBase); } \
+	FORCEINLINE static uint32 __PPO__m_treeBase() { return STRUCT_OFFSET(ATreeActor, m_treeBase); } \
+	FORCEINLINE static uint32 __PPO__m_tree_1() { return STRUCT_OFFSET(ATreeActor, m_tree_1); } \
+	FORCEINLINE static uint32 __PPO__m_tree_2() { return STRUCT_OFFSET(ATreeActor, m_tree_2); } \
+	FORCEINLINE static uint32 __PPO__m_treeTop() { return STRUCT_OFFSET(ATreeActor, m_treeTop); } \
+	FORCEINLINE static uint32 __PPO__m_dynamicTexture() { return STRUCT_OFFSET(ATreeActor, m_dynamicTexture); } \
+	FORCEINLINE static uint32 __PPO__m_dynamicMaterial() { return STRUCT_OFFSET(ATreeActor, m_dynamicMaterial); } \
+	FORCEINLINE static uint32 __PPO__m_dynamicMaterialInstance() { return STRUCT_OFFSET(ATreeActor, m_dynamicMaterialInstance); }
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_12_PROLOG
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_PRIVATE_PROPERTY_OFFSET \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_RPC_WRAPPERS \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_INCLASS \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_PRIVATE_PROPERTY_OFFSET \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_INCLASS_NO_PURE_DECLS \
+	Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Peck_GAM_415_Source_Peck_GAM_415_TreeActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
