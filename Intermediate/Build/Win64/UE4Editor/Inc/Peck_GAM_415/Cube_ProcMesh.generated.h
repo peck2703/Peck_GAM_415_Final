7f58916ed1eb6940c09056ae663555d8eae0b25d
peck2703 <mrpeckjr@gmail.com>
2019-01-26 20:30:27 -0600

    Bringing in system files through .gitignore (Testing)

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/Peck_GAM_415/Cube_ProcMesh.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PECK_GAM_415_Cube_ProcMesh_generated_h
+#error "Cube_ProcMesh.generated.h already included, missing '#pragma once' in Cube_ProcMesh.h"
+#endif
+#define PECK_GAM_415_Cube_ProcMesh_generated_h
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_RPC_WRAPPERS
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACube_ProcMesh(); \
+	friend PECK_GAM_415_API class UClass* Z_Construct_UClass_ACube_ProcMesh(); \
+public: \
+	DECLARE_CLASS(ACube_ProcMesh, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Peck_GAM_415"), NO_API) \
+	DECLARE_SERIALIZER(ACube_ProcMesh) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesACube_ProcMesh(); \
+	friend PECK_GAM_415_API class UClass* Z_Construct_UClass_ACube_ProcMesh(); \
+public: \
+	DECLARE_CLASS(ACube_ProcMesh, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Peck_GAM_415"), NO_API) \
+	DECLARE_SERIALIZER(ACube_ProcMesh) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACube_ProcMesh(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACube_ProcMesh) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACube_ProcMesh); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACube_ProcMesh); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACube_ProcMesh(ACube_ProcMesh&&); \
+	NO_API ACube_ProcMesh(const ACube_ProcMesh&); \
+public:
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACube_ProcMesh(ACube_ProcMesh&&); \
+	NO_API ACube_ProcMesh(const ACube_ProcMesh&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACube_ProcMesh); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACube_ProcMesh); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACube_ProcMesh)
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__mesh() { return STRUCT_OFFSET(ACube_ProcMesh, mesh); }
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_10_PROLOG
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_PRIVATE_PROPERTY_OFFSET \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_RPC_WRAPPERS \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_INCLASS \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_PRIVATE_PROPERTY_OFFSET \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_INCLASS_NO_PURE_DECLS \
+	Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Peck_GAM_415_Source_Peck_GAM_415_Cube_ProcMesh_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
